d7c794324e97870e687348a536665b141a0a68b5
Katrine-Marie <fjordscene@gmail.com>
2018-10-19 10:47:15 +0200

    Added the login form

--- pages/login.php
+++ pages/login.php
@@ -2,7 +2,17 @@
 	<h1>
 		Login
 	</h1>
-	
-	
-	
+
+	<form action="/code/logIn.php" method="post">
+		<div>
+			<label for="username">Username</label>
+			<input type="text" id="username" name="username" required>
+		</div>
+		<div>
+			<label for="password">Password</label>
+			<input type="password" id="password" name="password" required>
+		</div>
+		<input type="submit" name="submit" value="Log in">
+	</form>
+
 </div>
