f7fae3ecfb4e169de2ee7f815f0e58ac06eb3a7b
Katrine-Marie <fjordscene@gmail.com>
2018-10-23 15:07:24 +0200

    Added pages and include files for routing

---
+++ a-panel/pages/404.php
@@ -0,0 +1,5 @@
+<div class="main">
+	<h1>
+		Error 404 - Admin Page Not Found!
+	</h1>
+</div>
---
+++ a-panel/pages/home.php
@@ -0,0 +1,7 @@
+<div class="main">
+	<h1>
+		Control Panel - home
+	</h1>
+	<a href="../code/logOut.php">Log Out</a>
+
+</div>
